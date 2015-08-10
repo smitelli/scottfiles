500ca631ef3fdd8ff0b7e9a1728a18483c44a6c9
Scott Smitelli <scott@smitelli.com>
2015-08-10 16:29:43 -0400

    dc/vs database aliases

--- host.mov02/.bashrc.d/10-host.mov02.bash
+++ host.mov02/.bashrc.d/10-host.mov02.bash
@@ -1,1 +1,9 @@
+pwfile="${HOME}/.secrets/ssmitelli@$vs.db.devorch.com"
+if [[ -f "$pwfile" && -r "$pwfile" ]]
+then
+    alias "vsdb"="mysql -hvs.db.devorch.com -ussmitelli -p`cat $pwfile`"
+else
+    echo "Secret file $pwfile does not exist"
+fi
+
 HOSTNAME_COLOR=$COLOR_YELLOW
--- host.sc/.bashrc.d/10-host.sc.bash
+++ host.sc/.bashrc.d/10-host.sc.bash
@@ -1,1 +1,9 @@
+pwfile="${HOME}/.secrets/ssmitelli@$dc.db.devorch.com"
+if [[ -f "$pwfile" && -r "$pwfile" ]]
+then
+    alias "dcdb"="mysql -hdc.db.devorch.com -ussmitelli -p`cat $pwfile`"
+else
+    echo "Secret file $pwfile does not exist"
+fi
+
 HOSTNAME_COLOR=$COLOR_YELLOW
