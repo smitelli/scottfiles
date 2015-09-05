f7677be24969cc1ecfb33894943cfb7914a0de24
Scott Smitelli <scott@smitelli.com>
2015-09-04 23:21:29 -0400

    Don't work here anymore

--- host.mov02/.bashrc.d/10-host.mov02.bash
+++
@@ -1,8 +0,0 @@
-local pwfile="${HOME}/.secrets/ssmitelli@vs.db.devorch.com"
-if [[ -f "$pwfile" && -r "$pwfile" ]]; then
-    alias "vsdb"="mysql -hvs.db.devorch.com -ussmitelli -p`cat $pwfile`"
-else
-    echo "Secret file $pwfile does not exist"
-fi
-
-HOSTNAME_COLOR=$COLOR_YELLOW
