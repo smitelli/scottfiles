7e6b16c41a33b2f874fee68f6da13b094003224c
Scott Smitelli <scott@smitelli.com>
2018-11-27 22:08:37 -0500

    This has been driving me nuts lately

--- ssh/.bashrc.d/ssh.bash
+++ ssh/.bashrc.d/ssh.bash
@@ -3,7 +3,7 @@ SSH_AGENT_ENV="$HOME/.ssh/agent_env"
 function start_agent {
     [[ -v SCOTTFILES_DEBUG ]] && (echo; echo -n 'Starting new ssh-agent...')
 
-    (umask 077; ssh-agent -s -t 1800 | sed 's/^echo/#echo/' > "$SSH_AGENT_ENV")
+    (umask 077; ssh-agent -s -t 28800 | sed 's/^echo/#echo/' > "$SSH_AGENT_ENV")
     . "$SSH_AGENT_ENV" > /dev/null
 
     [[ -v SCOTTFILES_DEBUG ]] && echo ' done.'
