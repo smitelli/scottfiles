e53fef82b1fcf77d9de24d570ea8ad3e1a6e6b51
Scott Smitelli <scott@smitelli.com>
2022-07-09 22:34:08 -0400

    Brew completions look to be extremely broken now

--- homebrew/.bashrc.d/homebrew.bash
+++ homebrew/.bashrc.d/homebrew.bash
@@ -1,7 +1,16 @@
 if [ -x /opt/homebrew/bin/brew ]; then
     eval "$(/opt/homebrew/bin/brew shellenv)"
 
+    # https://docs.brew.sh/Analytics
     export HOMEBREW_NO_ANALYTICS=1
 
-    trysource_all "$(brew --prefix)/etc/bash_completion.d"
+    # https://docs.brew.sh/Shell-Completion
+    HOMEBREW_PREFIX="$(brew --prefix)"
+    if [[ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]; then
+        source "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
+    else
+        for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*; do
+            [[ -r "${COMPLETION}" ]] && source "${COMPLETION}"
+        done
+    fi
 fi
