3209e8fb0384780e0751c28313e2c825b07f78fd
Scott Smitelli <scott@smitelli.com>
2022-07-09 21:50:48 -0400

    Initialize brew the more honorable way

--- homebrew/.bashrc.d/homebrew.bash
+++ homebrew/.bashrc.d/homebrew.bash
@@ -1,4 +1,7 @@
-if has brew; then
-    export PATH="/usr/local/sbin:$PATH"
+if [ -x /opt/homebrew/bin/brew ]; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+
+    export HOMEBREW_NO_ANALYTICS=1
+
     trysource_all "$(brew --prefix)/etc/bash_completion.d"
 fi
