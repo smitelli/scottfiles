481167c2ee07d5c9617f5911b6cb18535f6407f3
Scott Smitelli <scott@smitelli.com>
2024-09-27 22:49:41 -0400

    Bug in zsh prompt escaping

--- prompt/.zshrc.d/prompt.zsh
+++ prompt/.zshrc.d/prompt.zsh
@@ -3,7 +3,7 @@ if [[ "$CLICOLOR" -eq 1 ]]; then
     export PS1="[\
 %{\$(_ps1_user_color)%}%n%{${COLOR_NC}%}@\
 %{\$(_ps1_host_color)%}%m%{${COLOR_NC}%}:\
-%{${COLOR_CYAN}%1~${COLOR_NC}%}]%# "
+%{${COLOR_CYAN}%}%1~%{${COLOR_NC}%}]%# "
 else
     export PS1='[%n@%m:%1~]%# '
 fi
