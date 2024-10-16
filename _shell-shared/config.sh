8bacf03a985d1a1417ad5c118da1c01914ec79b1
Scott Smitelli <scott@smitelli.com>
2024-10-15 21:48:05 -0400

    Add line count and progress estimate to `man` invoking `less`

--- _shell-shared/config.sh
+++ _shell-shared/config.sh
@@ -6,3 +6,16 @@
 
 # Uncomment the following line if there is any question about what's going on.
 # export SCOTTFILES_DEBUG=1
+
+# MANPAGER
+#       If $MANPAGER or $PAGER is set ($MANPAGER is used in preference), its
+#       value is used as the name of the program used to display the manual
+#       page. By default, less is used, falling back to cat if less is not found
+#       or is not executable.
+#
+#       The value may be a simple command name or a command with arguments, and
+#       may use shell quoting (backslashes, single quotes, or double quotes). It
+#       may not use pipes to connect multiple commands; if you need that, use a
+#       wrapper script, which may take the file to display either as an argument
+#       or on standard input.
+export MANPAGER='less +Gg'
