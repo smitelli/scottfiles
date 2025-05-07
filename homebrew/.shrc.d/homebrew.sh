db363947998b1c92e19a5fc7f345b13d47c8396a
Scott Smitelli <scott@smitelli.com>
2025-05-06 21:56:53 -0400

    Brew path shenanigans on Apple Silicon

--- homebrew/.shrc.d/homebrew.sh
+++ homebrew/.shrc.d/homebrew.sh
@@ -1,6 +1,12 @@
-if has brew; then
-    eval "$(brew shellenv)"
+if [ -f /opt/homebrew/bin/brew ]; then
+    # Apple Silicon
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+elif  [ -f /usr/local/bin/brew ]; then
+    # Intel
+    eval "$(/usr/local/bin/brew shellenv)"
+fi
 
+if has brew; then
     # https://docs.brew.sh/Analytics
     export HOMEBREW_NO_ANALYTICS=1
 fi
